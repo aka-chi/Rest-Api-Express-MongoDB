7af136c7c8dc123e0368697b3f81ceccf88c549a
akachi <Prokason@gmail.com>
2022-05-25 01:21:34 +0100

    Initial commit

---
+++ models/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    course: {
+        required: true,
+        type: String
+    },
+    year: {
+        required: true,
+        type: String
+    }
+    
+})
+
+module.exports = mongoose.model('Data', dataSchema)
